c15e903670cc0214ca363d1df9caed6a4b6831b2
Jorge Vazquez <jorge.vazquez.acevedo@alumnos.upm.es>
2020-01-31 09:34:21 +0100

    Setup Spring

---
+++ src/main/java/ingsoft1920/dho/DhoApplication.java
@@ -0,0 +1,13 @@
+package ingsoft1920.dho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ingsoft1920/dho/DhoApplicationTests.java
@@ -0,0 +1,13 @@
+package ingsoft1920.dho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
