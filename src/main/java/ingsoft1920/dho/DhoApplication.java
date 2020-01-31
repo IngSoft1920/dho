ba637b04d55e19abc02a15c3d4f810a6bc830ca5
Jorge Vazquez <jorge.vazquez.acevedo@alumnos.upm.es>
2020-01-31 15:42:29 +0100

    Incluir Logger + Test ejemplo

--- src/main/java/ingsoft1920/dho/DhoApplication.java
+++ src/main/java/ingsoft1920/dho/DhoApplication.java
@@ -1,12 +1,17 @@
 package ingsoft1920.dho;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DhoApplication {
-
+	final static Logger logger = LogManager.getLogger(DhoApplication.class.getName());
+	
 	public static void main(String[] args) {
+		logger.warn("Aplicacion iniciada");
 		SpringApplication.run(DhoApplication.class, args);
 	}
 
