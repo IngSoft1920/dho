1a0a571f5e5bf1f364011979f9178463b200d5a4
Alejandro Gil Ferrer <alejandro.gil.ferrer@gmail.com>
2020-02-17 10:31:40 +0100

    Controlador prueba

---
+++ src/main/java/ingsoft1920/controller/ControladorDHO.java
@@ -0,0 +1,35 @@
+package ingsoft1920.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import ingsoft1920.bean.Reserva;
+
+@Controller
+public class ControladorDHO {
+	
+	@GetMapping("/homePageDHO")
+	public String homePageDHO() {
+		return "homePageDHO.jsp";
+		
+	}
+	
+	@GetMapping("/homePageDHO/hacerReserva")
+	public String hsacerReserva() {
+		return "homePageDHO/hscerReserva.jsp";
+		
+	}
+	
+	@PostMapping("/homePageDHO/hacerReserva")
+	public String hsacerReservaSolicitud(Reserva reserva) {
+		System.out.println("reserva recibida:\n"+reserva);
+		return "homePageDHO/hacerReserva.jsp";
+	}
+	
+	
+	
+	
+
+}
