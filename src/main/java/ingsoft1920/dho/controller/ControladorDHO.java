e71950161897b3979c9ed9f7c2ae9a3e92d319ef
Alejandro Gil Ferrer <alejandro.gil.ferrer@gmail.com>
2020-02-20 20:17:24 +0100

    quitar respomseBody

--- src/main/java/ingsoft1920/dho/controller/ControladorDHO.java
+++ src/main/java/ingsoft1920/dho/controller/ControladorDHO.java
@@ -11,31 +11,26 @@ import ingsoft1920.dho.bean.ReservaBean;
 
 @Controller
 public class ControladorDHO {
-	@ResponseBody
 	@GetMapping("/homePageDHO")
 	public String homePageDHO() {
 		return "homePageDHO";
 		
 	}
-	@ResponseBody
 	@GetMapping("/homePageDHO/menu")
 	public String menu() {
 		return "menu";
 		
 	}
-	@ResponseBody
 	@GetMapping("/homePageDHO/menu/checkin")
 	public String checkin() {
 		return "checkin";
 		
 	}
-	@ResponseBody
 	@GetMapping("/homePageDHO/menu/checkout")
 	public String checkout() {
 		return "checkout";
 		
 	}
-	@ResponseBody
 	@PostMapping("/homePageDHO/hacerReserva")
 	public String hacerReservaSolicitud(ReservaBean reserva) {
 		System.out.println("reserva recibida:\n"+reserva);
