10ae80a79ada45590454c06b686d160579dbbd65
Maria Merino <m.merinop@alumnos.upm.es>
2020-03-05 10:47:59 +0100

    cambios en la base de datos

--- src/main/java/ingsoft1920/dho/bean/ServiciosDelHotelBean.java
+++ src/main/java/ingsoft1920/dho/bean/ServiciosDelHotelBean.java
@@ -24,9 +24,9 @@ public class ServiciosDelHotelBean {
 	//fijamos una disponibilidad
 	//este campo se va a ir actualiando segun nos vayan reservando(se ira decrementando segun
 	//reserven servicios)
-	private int DisponibilidaTotal;
+	private int DisponibilidadTotal;
 
-	
+	private int hotel_id;
 	
 	
 	
@@ -36,12 +36,13 @@ public class ServiciosDelHotelBean {
 
 
 	public ServiciosDelHotelBean(int id_ServicioHotel, Time horaInicioServicio, Time horaFinServicio,
-			int disponibilidadTotal) {
+			int disponibilidadTotal, int hotel_id) {
 		
 		this.id_ServicioHotel = id_ServicioHotel;
 		HoraInicioServicio = horaInicioServicio;
 		HoraFinServicio = horaFinServicio;
-		disponibilidadTotal = disponibilidadTotal;
+		this.DisponibilidadTotal = disponibilidadTotal;
+		this.hotel_id=hotel_id;
 	}
 
 
@@ -76,12 +77,12 @@ public class ServiciosDelHotelBean {
 
 
 	public int getDisponibilidadTotal() {
-		return DisponibilidaTotal;
+		return DisponibilidadTotal;
 	}
 
 
 	public void setDisponibilidadTotal(int disponibilidadTotal) {
-		disponibilidadTotal = DisponibilidaTotal;
+		this.DisponibilidadTotal=disponibilidadTotal;
 	}
 
 
@@ -89,7 +90,17 @@ public class ServiciosDelHotelBean {
 	public String toString() {
 		return "ServiciosDelHotelBean [id_ServicioHotel=" + id_ServicioHotel + ", HoraInicioServicio="
 				+ HoraInicioServicio + ", HoraFinServicio=" + HoraFinServicio + ", DisponibilidaTotal="
-				+ DisponibilidaTotal + "]";
+				+ DisponibilidadTotal + "]";
+	}
+
+
+	public int getHotel_id() {
+		return hotel_id;
+	}
+
+
+	public void setHotel_id(int hotel_id) {
+		this.hotel_id = hotel_id;
 	}
 	
 	
