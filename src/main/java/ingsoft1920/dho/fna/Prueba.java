03fa3b638786a046fb98f39ee7425a7d0663daa3
Sergio Carulli <sergiomario.carulli.perez@alumnos.upm.es>
2020-03-15 17:30:17 +0100

    fecha actual

--- src/main/java/ingsoft1920/dho/fna/Prueba.java
+++ src/main/java/ingsoft1920/dho/fna/Prueba.java
@@ -1,22 +1,16 @@
 package ingsoft1920.dho.fna;
 
+
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.sql.Date;
 import java.util.UUID;
 
 import com.itextpdf.text.Paragraph;
 
 public class Prueba {
 	public static void main(String[] args) {
-		/*
-    	for (FacturaBean elem: FacturaDAO.todasFacturasCliente(1)) {
-    		System.out.println(elem.toString());
-    	}
-    	*/
-
-		ArrayList<String> lista = new ArrayList<String>();
-		for (int i = 0; i < 20; i++) {
-		lista.add(UUID.randomUUID().toString());
-		}
 	}
 
 }
