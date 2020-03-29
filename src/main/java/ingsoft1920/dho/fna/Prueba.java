55cd289e7c13ebaa7f09c01aca622f884cd8c3aa
Sergio Carulli <sergiomario.carulli.perez@alumnos.upm.es>
2020-03-29 13:48:07 +0200

    subir PDF prueba

--- src/main/java/ingsoft1920/dho/fna/Prueba.java
+++ src/main/java/ingsoft1920/dho/fna/Prueba.java
@@ -4,15 +4,21 @@ package ingsoft1920.dho.fna;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.io.File;
 import java.sql.Date;
 import java.util.UUID;
 
 import com.itextpdf.text.Paragraph;
 
 public class Prueba {
-	public static void main(String[] args) {
-		ArchivosFacturaBean archivo = ArchivosFacturaDAO.getPDFByCod(1); 
-		System.out.println(archivo.toString()); 
+	public static void main(String[] args) throws Exception {
+        ArchivosFacturaBean archivo = ArchivosFacturaDAO.getPDFByCod(1);
+        if (archivo == null)
+            throw new Exception("Pdf no registrado en BBDD.");
+        // Abrir fichero pedido
+        File f = new File("files//archivo.pdf");
+        if (!f.exists())
+            throw new Exception("PDF does not exist.");
 	}
 
 }
