6a3ba1753f715a07257591a8dc7b773c8fe2f6a8
Sergio Carulli <sergiomario.carulli.perez@alumnos.upm.es>
2020-04-26 12:23:45 +0200

    Cambio Pdfs

--- src/main/java/ingsoft1920/dho/fna/PDF.java
+++ src/main/java/ingsoft1920/dho/fna/PDF.java
@@ -43,7 +43,7 @@ import ingsoft1920.dho.controller.Conexion;
  */
 
 @Controller
-public class generatePDFFileIText {
+public class PDF {
 	// Fonts definitions (Definición de fuentes).
 	private static final Font titleFont = FontFactory.getFont(FontFactory.COURIER_OBLIQUE, 26, Font.ITALIC);
 	private static final Font chapterFont = FontFactory.getFont(FontFactory.HELVETICA, 26, Font.BOLDITALIC);
@@ -78,7 +78,7 @@ public class generatePDFFileIText {
 		// We create the document and set the file name.        
 		// Creamos el documento y generamos el nombre del fichero.
 		String name=UUID.randomUUID().toString();
-		File file=new File("/hs/dho/files/"+name+".pdf");
+		File file=new File("C:/Users/sergi/OneDrive/Desktop/files/"+name+".pdf");
 		Date date=new Date(LocalDate.now().getYear()-1900,LocalDate.now().getMonthValue()-1,LocalDate.now().getDayOfMonth());
 		try {
 			Document document = new Document();
@@ -223,12 +223,10 @@ public class generatePDFFileIText {
 	 * @param args the command line arguments
 	 */
 
-	@ResponseBody
-	@GetMapping("/generatePDF/{cliente_id}")
-	public String generatePDF(@PathVariable("cliente_id") int cliente_id) {
-		generatePDFFileIText generatePDFFileIText = new generatePDFFileIText();
-		generatePDFFileIText.createPDF(cliente_id);
-		return "";
+	public static void main(String args[]) {
+		PDF generatePDFFileIText = new PDF();
+		generatePDFFileIText.createPDF(3);
+		System.out.println("PDF generado con exito");
 
 	}
 }
