597a42f6b094dd0559605d313fe2698fd0b66589
Javier <j.herraez@alumnos.upm.es>
2020-05-15 13:41:22 +0200

    api recibir proveedores mas productos

--- src/main/java/ingsoft1920/dho/DAO/ProductosDAO.java
+++ src/main/java/ingsoft1920/dho/DAO/ProductosDAO.java
@@ -1,5 +1,10 @@
 package ingsoft1920.dho.DAO;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import ingsoft1920.dho.bean.ProductosBean;
+import ingsoft1920.dho.bean.ProveedoresBean;
 import ingsoft1920.dho.controller.Conexion;
 
 public class ProductosDAO {
@@ -7,6 +12,37 @@ public class ProductosDAO {
 	
 	public ProductosDAO(Conexion conexion) {
 		this.conexion = conexion;
+	}
+	public static void anadirProducto(ProductosBean producto) {
+
+		if (conexion.getConexion() == null)
+			conexion.conectar();
+
+		PreparedStatement stm = null;
+		try {
+
+			stm = conexion.getConexion().prepareStatement("INSERT INTO Productos values (?,?,?,?,?)");
+			stm.setInt(1, producto.getProducto_id());
+			stm.setString(2, producto.getNombre());
+			stm.setInt(3, producto.getPrecio());
+			stm.setInt(3, producto.getProveedor_id());
+			stm.setString(2, producto.getUnidad());
+
+			stm.executeUpdate();
+		}
+
+		catch (SQLException ex) {
+			System.out.println("SQLException: " + ex.getMessage());
+		} finally { // it is a good idea to release resources in a finally block
+			if (stm != null) {
+				try {
+					stm.close();
+				} catch (SQLException sqlEx) {
+				}
+				stm = null;
+			}
+		}
+
 	}
 
 }
---
+++ src/main/java/ingsoft1920/dho/controller/pedirProovedores.java
@@ -0,0 +1,67 @@
+package ingsoft1920.dho.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import ingsoft1920.dho.DAO.ClienteDAO;
+import ingsoft1920.dho.DAO.EstanciaDAO;
+import ingsoft1920.dho.DAO.ProductosDAO;
+import ingsoft1920.dho.DAO.ProveedoresDAO;
+import ingsoft1920.dho.bean.ClienteBean;
+import ingsoft1920.dho.bean.ProductosBean;
+import ingsoft1920.dho.bean.ProveedoresBean;
+
+public class pedirProovedores {
+	/*
+	 * [ { "proveedor_id": 1,"empresa":"abc", "productos": [ { "id": 10, "nombre": "tomates",
+	 * "precio_venta": 300, "unidad_medida": "tonelada" }, { "id": 11, "nombre":
+	 * "lechuga", "precio_venta": 10, "unidad_medida": "kilo" } ] }, {
+	 * "proveedor_id": 3,"empresa":"cde", "productos": [ { "id": 20, "nombre": "vino",
+	 * "precio_venta": 40, "unidad_medida": "litro" }, { "id": 21, "nombre": "ron",
+	 * "precio_venta": 80, "unidad_medida": "litro" }, { "id": 21, "nombre": "ron",
+	 * "precio_venta": 80, "unidad_medida": "litro" } ] } ]
+	 */
+
+	public static ClienteBean pedirProovedores(int hotel_id) {
+		try {
+			// construimos la peticion
+			HttpClient client = new HttpClient("http://piedrafita.ls.fi.upm.es:7000/hotel-proovedores/" + hotel_id,
+					"GET");
+
+			int respCode = client.getResponseCode();
+			if (respCode == 200) {
+				String resp = client.getResponseBody();
+				JsonArray obj = (JsonArray) JsonParser.parseString(resp);
+				for (int i = 0; i < obj.size(); i++) {
+					JsonObject aux = obj.get(i).getAsJsonObject();
+					int proveedor_id = aux.get("proveedor_id").getAsInt();
+					String empresa=aux.get("empresa").getAsString();
+					ProveedoresBean proveedor=new ProveedoresBean(proveedor_id,empresa,hotel_id);
+					ProveedoresDAO.anadirProveedor(proveedor);
+					JsonArray productos = aux.get("productos").getAsJsonArray();
+					for (int j= 0;j<productos.size();j++) {
+						int id = aux.get("id").getAsInt();
+						String nombre = aux.get("nombre").getAsString();
+						int precio_venta = aux.get("precio_venta").getAsInt();
+						String unidad_medida = aux.get("unidad_medida").getAsString();
+						ProductosBean producto=new ProductosBean(id,nombre,precio_venta,proveedor_id,unidad_medida);
+						ProductosDAO.anadirProducto(producto);
+						
+					}
+				}
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+
+	}
+
+}
