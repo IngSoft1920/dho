3dd1fb4d4cddc53dd36a6477f3bc44de5fcd4f25
angel higuera <a.higuera@alumnos.upm.es>
2020-05-19 17:44:46 +0200

    PasarPedidosFnb

--- src/main/java/ingsoft1920/dho/controller/PasarPedidos.java
+++ src/main/java/ingsoft1920/dho/controller/PasarPedidos.java
@@ -14,18 +14,14 @@ import ingsoft1920.dho.bean.ProductosBean;
 public class PasarPedidos {
 
 	
-	/*Por lo que tiendo, el metodo debería recibir la lista de pedidos ya conformada, 
-	 * los campos que tienen que venir rellenados en cada uno de estos pedidos es: 
-	 * -producto_id, -cantidad,-fecha,-lugar,-especificaciones
-	 * Y el id_hotel que va a relaizar estos pedidos
-	 */
+
 	
-	public static void  pasarPedidos( int hotel_id ,LocalDate fecha,String lugar, int [] productos_id,
+	public static void  pasarPedidos( int hotel_id ,LocalDate fecha,String lugar, int [] productos_id,String [] nombresProductos,
 			int[] cantidades, String[] especificaciones) {
 		
 			
 			if (lugar.equals("restaurante")) {
-				pasarPedidosaFnb(hotel_id ,fecha, productos_id,
+				pasarPedidosaFnb(hotel_id ,fecha, nombresProductos,
 						cantidades, especificaciones);
 			}
 			pasarPedidosaCM(hotel_id ,fecha, productos_id,
@@ -88,8 +84,54 @@ public class PasarPedidos {
 		
 	}
 	
-	public static void pasarPedidosaFnb( int hotel_id ,LocalDate fecha, int[] productos,int[] cantidades,
+	public static void pasarPedidosaFnb( int hotel_id ,LocalDate fecha, String[] productos,int[] cantidades,
 			String[] especificaciones) {
+		
+		
+		try {
+			//construimos la peticion 
+			HttpClient client= new HttpClient("http://piedrafita.ls.fi.upm.es:7000/cliente","POST");
+			
+			
+			//metemos en el cuerpo de la peticion el id_hotel
+			
+			JsonObject param= new JsonObject();
+			
+			param.addProperty("fecha", fecha.toString());
+			
+			param.addProperty("hotel_id", hotel_id);
+			
+			
+			JsonArray lista = new JsonArray();
+			
+			int cont = 0;
+			
+			for(String producto: productos ) {
+				JsonObject obj = new JsonObject();
+				
+				obj.addProperty("producto_id", producto);
+				obj.addProperty("cantidad", cantidades[cont]);
+				obj.addProperty("especificaciones", especificaciones[cont]);
+				
+				lista.add(obj);
+			}
+			
+			param.add("productos", lista);
+			
+			
+			client.setRequestBody(param.toString());
+			
+		
+			
+							
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+		
+		
 		
 	}
 	
